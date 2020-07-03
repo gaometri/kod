84c9b53df265ab79b4de46e35c33d952d84d0401
gaometri <37038820+gaometri@users.noreply.github.com>
2020-07-03 12:22:33 +0300

    Update reklamsiz.js

--- reklamsiz.js
+++ reklamsiz.js
@@ -1,7 +1,7 @@
 FWDRAPUtils.onReady(function(){
    new FWDRAP({
     instanceName:"fwdrapPlayer0",
-        parentId:"fwdrapDiv0",
+        parentId:"menu-m",
        playlistsId:"fwdrapMainPlaylist0",
        mainFolderPath:"https://www.webdesign-flash.ro/p/rap/content/",
     skinPath:"modern_skin_white",
